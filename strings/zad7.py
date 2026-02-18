15dff387fc5c4f09d09a9b16d8cde66561f5e33b
Milosz <czerw.milosz@gmail.com>
2026-02-18 22:14:27 +0100

    Delete strings/zad7.py

--- strings/zad7.py
+++
@@ -1,8 +0,0 @@
-def bmi_calc(weight: float, height: float):
-    bmi = round((weight / height ** 2),2)
-    return f'Twoje BMI: {bmi}'
-
-user_weight = float(input('Podaj swoja wage [kg]: '))
-user_height = float(input('Podaj swoj wzrost [m]: '))
-
-print(bmi_calc(user_weight, user_height))
