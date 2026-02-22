59d8679ab7d1d26c4de07132071cc0f8efdbfbf3
czerwMilosz <czerw.milosz@gmail.com>
2026-02-22 12:47:47 +0100

    refactor: changed function name to format_text

--- strings/e31/zad5.py
+++ strings/e31/zad5.py
@@ -6,7 +6,7 @@ def add_white_space(text, spaces = 5):
    text_with_whitespace = ' ' * spaces + text
    return text_with_whitespace
 
-def print_text(whitespace_text, strip_text):
+def format_text(whitespace_text, strip_text):
     return f'''Tekst z bialymi znakami: {whitespace_text}
 Tekst po usunieciu bialych znakow: {strip_text}
 '''
@@ -15,6 +15,6 @@ def main():
     user_input = get_input_from_user()
     whitespace_text = add_white_space(user_input)
     strip_text = whitespace_text.lstrip()
-    print(print_text(whitespace_text, strip_text))
+    print(format_text(whitespace_text, strip_text))
 
 main()
