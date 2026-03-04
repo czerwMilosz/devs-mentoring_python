c0ad9f4386aae726af3a370774666625c00d3598
czerwMilosz <czerw.milosz@gmail.com>
2026-03-04 19:18:39 +0100

    poprawki zad7 zad9

--- strings/e1-3/zad9.py
+++ strings/e1-3/zad9.py
@@ -1,11 +1,28 @@
 def change_string(word: str):
-    if word[-1] in '!?.': #sprawdza czy ostatni element stringa konczy sie zaakiem int.
+    if word.endswith(('!', '?', '.')): #sprawdza czy ostatni element stringa konczy sie zaakiem int.
         new_word = word[-2] + word[1:-2] + word[0] + word[-1]
-    elif word[-1] == " ": #sprawdza czy ostatni element jest pustym znakiem
+    elif word.endswith(' '): #sprawdza czy ostatni element jest pustym znakiem
         new_word = word[-2] + word[1:-2] + word[0]
     else:
         new_word = word[-1] + word[1:-2] + word[0]
+    word.endswith(' ')
     return new_word
 
+def change_string_v2(word: str):
+    clean = word.translate(str.maketrans('', '', '!? .'))
+    changed = clean != word
+    if len(clean) > 1:
+        first, *middle, last = clean
+        clean = last + "".join(middle) + first
+    return clean + "." * changed
+
+
+
 user_word = str(input('Wprowadz tekst: '))
-print(change_string(user_word))
+#print(change_string(user_word))
+print(change_string_v2(user_word))
+
+
+
+# matching pattern poczytac https://peps.python.org/pep-0634/
+# https://realpython.com/structural-pattern-matching/
