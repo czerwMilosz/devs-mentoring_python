7bd5e700c4229b105f6a93f0dcd9151c6423f5f3
czerwMilosz <czerw.milosz@gmail.com>
2026-03-04 19:20:41 +0100

    zad4 poprawki

--- strings/e31/zad4.py
+++ strings/e31/zad4.py
@@ -7,24 +7,24 @@ def pij_mleko():
             return text
 
 
-def bedziesz_wielki(text):
+def bedziesz_wielki(text: str) -> str:
     lower_text = text.lower()
     check_string = ''
 
-    for l in lower_text: #sprawdza czy tekst zawiera litery do powiekszenia
-        if l.isalpha():  #na poczatku chcialem tu uzyc zakresu  i kodow ASCII z ord() ale to nie wykrywalo polskich znakow
+    for l in lower_text: # sprawdza czy tekst zawiera litery do powiekszenia
+        if l.isalpha():  # na poczatku chcialem tu uzyc zakresu  i kodow ASCII z ord() ale to nie wykrywalo polskich znakow
             break
         else:
             check_string += l
 
     if lower_text == check_string:
         return f'Tekst nie zawiera liter, to czego sie innego sie spodziewales: {lower_text}'
-    else:
-        return f'Twoj powiekszony tekst: {lower_text.upper()}'
+    return f'Twoj powiekszony tekst: {lower_text.upper()}'
 
 def main():
     user_input = pij_mleko()
     result = bedziesz_wielki(user_input)
     print(result)
 
-main()
+if __name__ == '__main__':
+    main()
