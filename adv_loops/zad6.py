b227bedb119f59f8828c34bd371acacb0bb7b491
czerwMilosz <czerw.milosz@gmail.com>
2026-03-27 19:04:37 +0100

    devs-mentoring notes print f

--- adv_loops/zad6.py
+++ adv_loops/zad6.py
@@ -1,3 +1,5 @@
+from collections import Counter
+import datetime
 NUMBER = 1235555
 
 def split_number(number: int) -> list[int]:
@@ -12,10 +14,38 @@ def count_digits(digits: list) -> dict[int, int]:
     return digit_counter
 
 def main():
-    digits = split_number(NUMBER)
-    digit_counter = count_digits(digits)
-    for digit, count in digit_counter.items():
-        print(f"{digit}: {count}")
+    # digits = split_number(NUMBER)
+    # digit_counter = count_digits(digits)
+    # for digit, count in digit_counter.items():
+    #     print(f"{digit}: {count}")
+    text_number = str(NUMBER)
+    print(dict(Counter(text_number)))
+    print({d: text_number.count(d) for d in set(text_number)})
+    print({k:v for k,v in {str(d): text_number.count(str(d)) for d in range(10)}.items() if v != 0})
+    name = "Jan"
+    age = 20
+    a = 5
+    b = 3
+    pi = 3.1415926535
+    n = 1000000
+    m = 255
+    c = 0.314
+    now = datetime.datetime.now()
+    print(f"my name is {name=} and im {age} years old")
+    print(f"{a} + {b} = {a+b}")
+    print(f"{a + b =}")
+    print(f"{pi:.2f}")
+    print(f"{pi:<15} {n:_}")
+    print(f"{b:15}")
+    print(f"{c:.2%}")
+    print(f"{m:b}")
+    print(f"{m:o}")
+    print(f"{m:x}")
+    print(f"{m:X}")
+    print(f"{now:%Y-%m-%d}")
+
+    #zrobic tabelke z jsona
+
 
 
 if __name__ == "__main__":
